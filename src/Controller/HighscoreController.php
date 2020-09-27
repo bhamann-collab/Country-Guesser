0ffdf600ecde6f7804921a6a212353979193ecdc
bhamann-collab <brockohamann@gmail.com>
2020-09-27 20:50:09 +0930

    we are now validating objects in the HighscoreController, symfony/validator package has been installed

--- src/Controller/HighscoreController.php
+++ src/Controller/HighscoreController.php
@@ -6,13 +6,14 @@ use App\Entity\Highscore;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class HighscoreController extends ApiController
 {
     /**
      * @Route("/highscore", name="highscore")
      */
-    public function createHighscore(EntityManagerInterface $entityManager): Response
+    public function createHighscore(EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
         //gets Doctrine's entity manager object, which is the most important object in Doctrine. It's responsible for saving objects to, and fetching objects from, the database.
         //$entityManager = $this->getDoctrine()->getManager();
@@ -26,6 +27,11 @@ class HighscoreController extends ApiController
 
         //actally executes the queries (i.e. the INSERT query)
         $entityManager->flush();
+
+        $errors = $validator->validate($highscore);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
 
         return new Response('Saved new highscore with id '.$highscore->getId());
     }
