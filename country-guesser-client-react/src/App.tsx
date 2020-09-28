ea48e71d650608b1cfc14b7960b7a789a3b7f525
bhamann-collab <brockohamann@gmail.com>
2020-09-28 11:06:16 +0930

    React Router is now workingh

--- country-guesser-client-react/src/App.tsx
+++ country-guesser-client-react/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import PageTitle from './component/PageTitle';
+import GuessArea from './component/GuessArea';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path='/' exact component={PageTitle} />
+                    <Route path='/guessArea' exact component={GuessArea} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ country-guesser-client-react/src/component/GuessArea.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GuessArea = () => {
+    return (
+        <div>
+            <h1>GuessArea</h1>
+        </div>
+    )
+}
+
+export default GuessArea
