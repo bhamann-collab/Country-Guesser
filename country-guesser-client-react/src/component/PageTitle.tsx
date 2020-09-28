e62953eb4528570181e8aa15ddfcf08762061363
bhamann-collab <brockohamann@gmail.com>
2020-09-28 11:44:14 +0930

    Getting react strap into the project

--- country-guesser-client-react/src/component/PageTitle.tsx
+++ country-guesser-client-react/src/component/PageTitle.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const PageTitle = () => {
     return (
-        <div>
-            <h1>PageTitle</h1>
+        <div className="pageTitle">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="titleText">
+                            <h1>Guess That Country!</h1>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>Answer the question with the country you think is more fitted. Get a streak going!</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
