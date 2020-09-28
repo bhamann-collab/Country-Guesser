bf208dc2c5c8611025243057a4c5c92d42ef82b9
bhamann-collab <brockohamann@gmail.com>
2020-09-28 13:07:41 +0930

    Bootstrap content on the GuessArea page

--- country-guesser-client-react/src/component/GuessArea.tsx
+++ country-guesser-client-react/src/component/GuessArea.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 const GuessArea = () => {
     return (
-        <div>
-            <h1>GuessArea</h1>
+        <div className="guessArea">
+            <Container>
+                <Row>
+                    <Col>
+                        Streak
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        question
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="4">
+                        <h1>Country</h1>
+                        <Button outline color="primary">Click Me</Button>
+                    </Col>
+                    <Col xs="4"></Col>
+                    <Col xs="4">
+                        <h1>Country</h1>
+                        <Button outline color="primary">Click Me</Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
