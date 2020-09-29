49245efd2c9526e502f35fdd6068655e373ccd79
bhamann-collab <brockohamann@gmail.com>
2020-09-29 11:21:07 +0930

    Gameover buttons

--- country-guesser-client-react/src/component/GuessArea/GameOver.tsx
+++ country-guesser-client-react/src/component/GuessArea/GameOver.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Container, Row, Col, Button } from 'reactstrap';
 
 const GameOver = () => {
     return (
-        <div>
-            GameOver
+        <div className="GameOver">
+            <h1>Game Over</h1>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <Button size="lg" color="success">Play Again</Button>
+                    </Col>
+                    <Col>
+                        <Button size="lg" color="info">Enter Score</Button>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     )
 }
