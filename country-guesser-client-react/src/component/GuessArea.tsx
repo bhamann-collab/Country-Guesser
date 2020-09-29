c2094ddefeb90d239fd7e7b97f5666a4ad7fc825
bhamann-collab <brockohamann@gmail.com>
2020-09-29 09:46:37 +0930

    Notify user whether they got the right answer or not

--- country-guesser-client-react/src/component/GuessArea.tsx
+++ country-guesser-client-react/src/component/GuessArea.tsx
@@ -1,11 +1,14 @@
-import Axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 
 const GuessArea = () => {
     const [firstCountry, setFirstCountry] = useState({'flag': "../../img/placeholder-flag.png"});
     const [secondCountry, setSecondCountry] = useState({'flag': "../../img/placeholder-flag.png"});
+    //1 means you are correct, 2 means you are incorrect
+    const [questionStatus, setQuestionStatus] = useState(0)
 
     useEffect(() => {
         let firstRandNum = Math.floor(Math.random() * 250);
@@ -13,7 +16,6 @@ const GuessArea = () => {
         axios
         .get(`https://restcountries.eu/rest/v2/all`)
         .then((res) => {
-            console.log(res);
             let countryName = res.data[firstRandNum].name;
             let countryPopulation = res.data[firstRandNum].population;
             let countryArea = res.data[firstRandNum].area;
@@ -48,7 +50,23 @@ const GuessArea = () => {
 
     }, [])
 
-    console.log(firstCountry)
+    function whoHasMorePeople(country1, country2) {
+        if (country1.population > country2.population) {
+            return country1.name;
+        } else {
+            return country2.name;
+        }
+    }
+
+    function handleclick(countryName) {
+        if (countryName === whoHasMorePeople(firstCountry, secondCountry)) {
+            toast.success("Correct!");
+        } else {
+            toast.error("Incorrect!");
+        }
+        console.log(countryName);
+        console.log(whoHasMorePeople(firstCountry, secondCountry));
+    }
 
     return (
         <div className="guessArea">
@@ -71,15 +89,24 @@ const GuessArea = () => {
                     <Col xs="5">
                         <div className="countryPosition">
                             <h1>{firstCountry.name}</h1>
-                            <img src={firstCountry.flag} alt="country"/>
+                            <img src={firstCountry.flag} onClick={() => handleclick(firstCountry.name)} alt="country"/>
                         </div>
                     </Col>
                     <Col xs="2"></Col>
                     <Col xs="5">
-                    <div className="countryPosition">
+                        <div className="countryPosition">
                             <h1>{secondCountry.name}</h1>
-                            <img src={secondCountry.flag} alt="country"/>
+                            <img src={secondCountry.flag} onClick={() => handleclick(secondCountry.name)} alt="country"/>
                         </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h1>Place Holder</h1>
+                        <ToastContainer 
+                        hideProgressBar={true}
+                        autoClose={2000}
+                        />
                     </Col>
                 </Row>
             </Container>
