f3a9885cb81758f5d5b1d367bdce675b0ca6a8d8
bhamann-collab <brockohamann@gmail.com>
2020-09-29 12:24:59 +0930

    modal is poping up when you want to type your name

--- country-guesser-client-react/src/component/GuessArea/GameOver.tsx
+++ country-guesser-client-react/src/component/GuessArea/GameOver.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { Container, Row, Col, Button } from 'reactstrap';
+import React, { useState } from 'react'
+import { Container, Row, Col, Button, Modal, ModalHeader, ModalBody, Input, Label, Form, FormGroup } from 'reactstrap';
 
 const GameOver = (props) => {
+    const [modal, setModal] = useState(false);
+    const [unmountOnClose, setUnmountOnClose] = useState(true);
+
+    const toggle = () => setModal(!modal);
+    const changeUnmountOnClose = e => {
+        let value = e.target.value;
+        setUnmountOnClose(JSON.parse(value));
+    }
+
     return (
         <div className="GameOver">
             <h1>Game Over</h1>
@@ -12,11 +21,17 @@ const GameOver = (props) => {
                         <Button size="lg" color="success" onClick={props.setNewCountries}>Play Again</Button>
                     </Col>
                     <Col>
-                        <Button size="lg" color="info">Enter Score</Button>
+                        <Button size="lg" color="info" onClick={toggle}>Enter Score</Button>
                     </Col>
                     <Col></Col>
                 </Row>
             </Container>
+            <Modal isOpen={modal} toggle={toggle} unmountOnClose={unmountOnClose}>
+                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                    <Input type="textarea" placeholder="Enter name"/>
+                </ModalBody>
+            </Modal>
         </div>
     )
 }
