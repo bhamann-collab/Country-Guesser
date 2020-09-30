d8a53e0e1e0d1d03334b7cadc0d71c64d11124b2
bhamann-collab <brockohamann@gmail.com>
2020-09-30 12:04:34 +0930

    added more test cases

--- tests/Util/CalculatorTest.php
+++ tests/Util/CalculatorTest.php
@@ -9,9 +9,11 @@ class CalculatorTest extends TestCase
     public function testAdd()
     {
         $calculator = new Calculator();
-        $result = $calculator->add(30, 12);
+        $test1 = $calculator->add(30, 12);
+        $test2 = $calculator->add(1, 2);
 
-        $this->assertEquals(42, $result);
+        $this->assertEquals(42, $test1);
+        $this->assertEquals(3, $test2);
     }
 }
 
