4e2c14fbf6cd3898efb6c6c70875a4bfc5ea2df3
bhamann-collab <brockohamann@gmail.com>
2020-09-30 12:46:26 +0930

    added a test case for the POST request

--- tests/Controller/HighscoreControllerTest.php
+++ tests/Controller/HighscoreControllerTest.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HighscoreControllerTest extends WebTestCase
 {
+    //Testing the GET request with the url extension: /showHighScore
     public function testShowPost()
     {
         $client = static::createClient();
@@ -12,5 +13,26 @@ class HighscoreControllerTest extends WebTestCase
         $client->request('GET', '/showHighScore');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    //Testing the POST request with the url extension: /createHighscore with test parameters
+    public function testPostRequest()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', 
+        '/createHighscore',
+        array(),
+        array(),
+        array('CONTENT_TYPE' => 'application/json'),
+        '{
+            
+            "nickname": "php testing",
+            "streak": 5
+            
+        }'
+    );
+
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
     }
 }
