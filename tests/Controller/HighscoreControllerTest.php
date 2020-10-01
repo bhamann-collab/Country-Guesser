80fb367147817da5d1e6ef4e91a56993e9defdb1
bhamann-collab <brockohamann@gmail.com>
2020-10-01 20:42:12 +0930

    Creating another test to test out @dataProvider

--- tests/Controller/HighscoreControllerTest.php
+++ tests/Controller/HighscoreControllerTest.php
@@ -35,4 +35,22 @@ class HighscoreControllerTest extends WebTestCase
 
         $this->assertEquals(201, $client->getResponse()->getStatusCode());
     }
+
+    //Testing the @dataProvider to determine if the GET request is successful
+    /**
+     * @dataProvider provideUrls
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+    public function provideUrls()
+    {
+        return [
+            ['/showHighScore']
+        ];
+    }
 }
