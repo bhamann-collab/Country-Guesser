ccfdada12409566a42073ca2f61eca22b1bf0c63
bhamann-collab <brockohamann@gmail.com>
2020-10-10 19:38:30 +1030

    Moving the whole folder from old-code to master

---
+++ src/Controller/HighscoreController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\HighscoreRepository;
+use App\Entity\Highscore;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class HighscoreController extends AbstractController
+{
+    private $highscoreRepository;
+
+    public function __construct(HighscoreRepository $highscoreRepository)
+    {
+        $this->highscoreRepository = $highscoreRepository;
+    }
+
+    /**
+    * @Route("/{reactRouting}", name="index", defaults={"reactRouting": null})
+    */
+    public function dashboard(): Response
+    {
+        return $this->render('pub/index.html.twig');
+    }
+
+    /**
+     * @Route("/createHighscore", name="highscore", methods={"POST"})
+     */
+    public function createHighscore(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        //gets Doctrine's entity manager object, which is the most important object in Doctrine. It's responsible for saving objects to, and fetching objects from, the database.
+        //$entityManager = $this->getDoctrine()->getManager();
+
+
+        $nickname = $data['nickname'];
+        $streak = $data['streak'];
+
+        if (empty($nickname) || empty($streak)) {
+            throw new NotFoundHttpException('Expection mandatory parameters!');
+        }
+
+        $this->highscoreRepository->saveScore($nickname, $streak);
+
+        return new JsonResponse(['status' => 'Customer created!'], Response::HTTP_CREATED);
+
+    }
+
+    /**
+     * @Route("/showHighScore", name="product_show", methods={"GET"})
+     */
+    public function show()
+    {
+        $highscore = $this->getDoctrine()
+            ->getRepository(Highscore::class)
+            ->findTopTen();
+
+        if (!$highscore) {
+            throw $this->createNotFoundException(
+                'There are no entries in the database'
+            );
+        }
+
+        //Getting the name of every highscore entry and bundeling then into an array
+        $data = [];
+        foreach ($highscore as $entry) {
+            $data[] = [
+                'name' => $entry->getNickname(),
+                'streak' => $entry->getStreak()
+            ];
+        }
+
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+}
